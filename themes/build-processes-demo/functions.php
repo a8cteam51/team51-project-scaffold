c51c71e1a2c436f65afa58f333eb7631bd3f43b0
Antonius Hegyes <tony.hegyes@automattic.com>
2022-10-21 10:46:29 +0200

    Add empty child theme

---
+++ themes/build-processes-demo/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Build processes demo theme functions and definitions.
+ *
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package BuildProcessesDemo_Theme
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
