d6f176e186d953316c7b20a599cb08e7122a472d
Antonius Hegyes <tony.hegyes@automattic.com>
2022-12-14 18:22:49 +0100

    Add missing scripts for extra functionality plugin

--- mu-plugins/build-processes-demo-extra-functionality/build-processes-demo-extra-functionality.php
+++ mu-plugins/build-processes-demo-extra-functionality/build-processes-demo-extra-functionality.php
@@ -1,14 +1,24 @@
 <?php
 /**
- * Plugin Name:       Build Processes Demo Extra Functionality
- * Description:       An extra functionality plugin for a custom theme. Could include post types, metaboxes etc. Do not use for blocks!
- * Requires at least: 6.1
- * Requires PHP:      8.1
- * Version:           0.1.0
- * Author:            WP Special Projects
- * License:           GPL-3.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:       build-processes-demo-extra-functionality
+ * The Build Processes Demo extra functionality plugin bootstrap file.
+ *
+ * @since       0.1.0
+ * @version     0.1.0
+ * @author      WP Special Projects
+ * @license     GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:             Build Processes Demo Extra Functionality
+ * Description:             An extra functionality plugin for a custom theme. Could include post types, metaboxes etc. Do not use for blocks!
+ * Version:                 0.1.0
+ * Requires at least:       6.1
+ * Requires PHP:            8.1
+ * Author:                  WP Special Projects
+ * Author URI:              https://wpspecialprojects.wordpress.com/
+ * License:                 GPL-3.0-or-later
+ * License URI:             https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:             build-processes-demo-extra-functionality
+ * Domain Path:             /languages
  */
 
 defined( 'ABSPATH' ) || exit;
