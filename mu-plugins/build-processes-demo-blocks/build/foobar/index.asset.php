68c0b1dac9e8878ad7b846ced2d31774066c6671
Fernando Fernandes <fernandofernand3s@gmail.com>
2022-12-15 22:45:44 -0300

    Build assets

--- mu-plugins/build-processes-demo-blocks/build/foobar/index.asset.php
+++ mu-plugins/build-processes-demo-blocks/build/foobar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'eb31e3a8a23be083063b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'dbf32498da98f7ec73e7');
