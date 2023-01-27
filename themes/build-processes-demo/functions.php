65aa5e4901613a330df9050a92a8ec4a0a3362c6
Chris Bell <chrisbcodeable@gmail.com>
2023-01-27 19:28:00 +0000

    feat: load stylesheet with cache busting

--- themes/build-processes-demo/functions.php
+++ themes/build-processes-demo/functions.php
@@ -11,6 +11,19 @@
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Loads theme styles and scripts.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function bpd_enqueue_scripts() {
+	// Enqueue the theme's stylesheet with cache busting.
+	wp_enqueue_style( 'build-processes-demo-style', get_stylesheet_uri(), array(), filemtime( get_stylesheet_directory() . '/style.css' ) );
+}
+add_action( 'wp_enqueue_scripts', 'bpd_enqueue_scripts' );
+
 // Load the theme's translated strings.
 function bpd_load_theme_textdomain(): void {
 	load_child_theme_textdomain( 'build-processes-demo', get_stylesheet_directory() . '/languages' );
